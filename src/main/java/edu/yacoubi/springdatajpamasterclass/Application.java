c3680ae9ff8771c47b53a928d5f9f46eacc1c5d3
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-10-04 01:17:43 +0200

    Exploring Some Repository Methods

--- src/main/java/edu/yacoubi/springdatajpamasterclass/Application.java
+++ src/main/java/edu/yacoubi/springdatajpamasterclass/Application.java
@@ -1,13 +1,74 @@
 package edu.yacoubi.springdatajpamasterclass;
 
+import edu.yacoubi.springdatajpamasterclass.model.Student;
+import edu.yacoubi.springdatajpamasterclass.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+		return args -> {
+
+			Student maria = new Student(
+					"Maria",
+					"Jones",
+					"maria.jones@yacoubi.edu",
+					25
+			);
+
+			Student ahmed = new Student(
+					"Ahmed",
+					"Ali",
+					"ahmed.ali@yacoubi.edu",
+					22
+			);
+			System.out.println("Adding maria & ahmed");
+			studentRepository.saveAll(List.of(maria,ahmed));
+
+			System.out.print("count number of students : ");
+			System.out.println(studentRepository.count());
+
+			System.out.print("Find student with ID 2 : ");
+			studentRepository
+					.findById(2L)
+					.ifPresentOrElse(
+							System.out::println,
+							() -> System.out.println("Student with ID 2 not found")
+					);
+
+			System.out.println();
+
+			System.out.print("Find student with ID 3 : ");
+			studentRepository
+					.findById(3L)
+					.ifPresentOrElse(
+							System.out::println,
+							() -> System.out.println("Student with ID 2 not found")
+					);
+
+			System.out.println();
+
+			System.out.println("Find all students :");
+			List<Student> students = studentRepository.findAll();
+			students.forEach(System.out::println);
+
+			System.out.println("Delete student with ID 1: ");
+			studentRepository.deleteById(1L);
+			System.out.print("count number of students : ");
+			System.out.println( studentRepository.count() );
+		};
 	}
 
 }
--- src/main/java/edu/yacoubi/springdatajpamasterclass/model/Student.java
+++ src/main/java/edu/yacoubi/springdatajpamasterclass/model/Student.java
@@ -18,7 +18,7 @@ import static javax.persistence.GenerationType.SEQUENCE;
               )
         }
 )
-@Data @AllArgsConstructor @NoArgsConstructor
+@Data @NoArgsConstructor
 public class Student {
     @Id
     @SequenceGenerator(
@@ -63,4 +63,11 @@ public class Student {
             nullable = false
     )
     private Integer age;
+
+    public Student(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
 }
