3fc7c4223309acb91955704ffb13253c837ee876
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-10-04 16:31:29 +0200

    how to write jpql or to overwrite qwery method to have the full control obose sql's

--- src/main/java/edu/yacoubi/springdatajpamasterclass/model/Student.java
+++ src/main/java/edu/yacoubi/springdatajpamasterclass/model/Student.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 
-@Entity(name = "Student")
+@Entity(name = "Student") // name to reference to it in JPQL
 @Table(
         name = "student",
         uniqueConstraints = {
@@ -64,7 +64,10 @@ public class Student {
     )
     private Integer age;
 
-    public Student(String firstName, String lastName, String email, Integer age) {
+    public Student(String firstName,
+                   String lastName,
+                   String email,
+                   Integer age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
--- src/main/java/edu/yacoubi/springdatajpamasterclass/repository/StudentRepository.java
+++ src/main/java/edu/yacoubi/springdatajpamasterclass/repository/StudentRepository.java
@@ -2,27 +2,37 @@ package edu.yacoubi.springdatajpamasterclass.repository;
 
 import edu.yacoubi.springdatajpamasterclass.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    // we can define a custom query (as apart from a predefined queries)
-    // as example how to find someone with a given email
-    // and a methods name follows the naming convention
-    // so the framework will generate for us the Query
+    // we can define a custom method query (as apart from a predefined queries)
+    // for example how to find someone with a given email.
+    // The method names follow naming convention
+    // and the framework will generate for us the sql query.
+    // But we can overwrite it, when want to have a full control above the sql,
+    // so we can annotate the method query w. @Query.
+    // In these case serves only how to do it.
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
     Optional<Student> findStudentByEmail(String email);
 
+    // the method query will work also without the
+    // annotation. (edu)
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age = ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeEquals(
             String firstName,
             Integer age
     );
 
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age > ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThan(
             String firstName,
             Integer age
     );
 
+    @Query("SELECT s FROM Student s WHERE s.firstName = ?1 AND s.age >= ?2")
     List<Student> findStudentsByFirstNameEqualsAndAgeIsGreaterThanEqual(
             String firstName,
             Integer age
